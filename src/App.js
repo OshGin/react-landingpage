77d3d0c8754010bcb2bc2d189d4e6d5f3be75210
Mathias Angerer <>
2022-01-25 13:12:13 +0100

    Set up red black box

--- src/App.js
+++ src/App.js
@@ -25,11 +25,11 @@ function App() {
   `;
   const blueboxPic = css`
     width: 395px; /* width of container */
-    height: 610px; /* height of container */
+    height: 600px; /* height of container */
     object-fit: cover;
     object-position: 20% 10%; /* try 20px 10px */
     padding-left: 12rem;
-    margin-bottom: -10rem;
+    margin-bottom: 0;
   `;
   const blueboxHone = css`
     color: white;
@@ -40,9 +40,39 @@ function App() {
     color: white;
     opacity: 0.5;
   `;
+  const redBox = css`
+    background-color: #f5375a;
+    display: flex;
+    flex-direction: row;
+    font-family: inherit;
+    line-height: inherit;
+    height: 600px;
+    width: 75% justify-content;
+    line-height: 24px;
+    padding-top: 0;
+    tab-size: 4;
+    text-size-adjust: 100%;
+    padding-left: 12rem;
+  `;
+  const blackBox = css`
+    background-color: black;
+    display: flex;
+    flex-direction: row;
+    font-family: inherit;
+    line-height: inherit;
+    height: 600px;
+    width: 75% justify-content;
+    line-height: 24px;
+    padding-top: 0;
+    tab-size: 4;
+    text-size-adjust: 100%;
+    padding-left: 12rem;
+    margin-bottom: 10;
+  `;
+
   return (
     <div className="App">
-      <div css={blueBox}>
+      <section css={blueBox}>
         <div css={bluetextBox}>
           <a href="https://apps.apple.com/app/triage-2/id1585295768">
             <img src="https://triage.cc/hero.svg" alt="button"></img>
@@ -63,8 +93,31 @@ function App() {
             alt="Screen"
           />
         </div>
-      </div>
-      <div>2</div>
+      </section>
+      <section css={redBox}>
+        <div>
+          <img
+            css={blueboxPic}
+            src="https://triage.cc/screenshots/archive.png"
+            alt="Screen"
+          />
+        </div>
+        <h1 css={blueboxHone}>Drag left to archive</h1>
+        <h2 css={blueboxHtwo}>
+          Unread messages appear in a stack of cards, like this.
+        </h2>
+      </section>
+      <section css={blackBox}>
+        <h1 css={blueboxHone}>Drag right to keep</h1>
+        <h2 css={blueboxHtwo}>Or create your own custom workflow.</h2>
+        <div>
+          <img
+            css={blueboxPic}
+            src="https://triage.cc/screenshots/keep.png"
+            alt="Screen"
+          />
+        </div>
+      </section>
       <div>3</div>
       <div>4</div>
       <div>5</div>
